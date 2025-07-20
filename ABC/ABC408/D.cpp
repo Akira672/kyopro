f36f704326c58b7f67538b71ece42dc9af0ae327
homePC <kitsugi-akira-sd@ynu.jp>
2025-07-20 15:57:10 +0900

    0720

--- ABC/ABC408/D.cpp
+++ ABC/ABC408/D.cpp
@@ -1,9 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    long long N,M;
-    cin >> N >> M;
+int main() {
+    int T;
+    cin >> T;
+    while(T--) {
+        int N;
+        cin >> N;
+        string S;
+        cin >> S;
+
+        vector<vector<int>> DP(3,vector<int>(N+1,0));
+        for(int i=0;i<N;i++) {
+            DP[0][i+1] = DP[0][i] + (S[i] == '1');
+            DP[1][i+1] = min(DP[0][i] + (S[i] == '0'),DP[1][i] + (S[i] == '0'));
+            DP[2][i+1] = min(DP[1][i] + (S[i] == '1'),DP[2][i] + (S[i] == '1'));
+        }
+
+        cout << min(DP[0][N],min(DP[1][N],DP[2][N])) << "\n";
+    }
 
     return 0;
 }
